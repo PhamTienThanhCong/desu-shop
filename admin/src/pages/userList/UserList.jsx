5c06f5f0e9987404f3e77f797871fee067325f1d
congphamtienthanh <congphamtienthanh@gmail.com>
2023-02-03 14:00:14 +0700

    up

--- admin/src/pages/userList/UserList.jsx
+++ admin/src/pages/userList/UserList.jsx
@@ -10,6 +10,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import OutlinedInput from '@mui/material/OutlinedInput';
 
 
 export default function UserList() {
@@ -20,6 +21,7 @@ export default function UserList() {
   // paging data user
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(10);
+  const [username, setUsername] = useState("")
 
   const getData = async () => {
     setLoading(true);
@@ -34,6 +36,10 @@ export default function UserList() {
     }
   }
 
+  const handleChangeUsername = (e) => {
+    setUsername(e.target.value);
+  }
+
   useEffect(() => {
     getData();
   }, []);
@@ -42,6 +48,7 @@ export default function UserList() {
     <div className="userList">
       <div className="userListTitleContainer">
         <h1 className="userListTitle">User List</h1>
+        <OutlinedInput onChange={ handleChangeUsername } placeholder="Search for username" />
         <Link to="/newUser">
           <button className="userAddButton">Create</button>
         </Link>
@@ -71,7 +78,13 @@ export default function UserList() {
               </TableRow>
             ) : (
               // data
-              data.slice((page - 1) * 10, page * 10).map((item) => (
+              data.filter((item) => {
+                if (username === "") {
+                  return item;
+                } else if (item.username.toLowerCase().includes(username.toLowerCase())) {
+                  return item;
+                }
+              }).reverse().slice((page - 1) * 10, page * 10).map((item) => (
                 <TableRow
                   key={item.id}
                   sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
